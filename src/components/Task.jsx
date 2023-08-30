dab5999204977ebb012f1c3266fb9b044773e816
juleni <69912179+juleni@users.noreply.github.com>
2023-08-30 15:20:22 +0200

    Added Task component + styles

---
+++ src/components/Task.jsx
@@ -0,0 +1,18 @@
+import classNames from "classnames";
+import { useStore } from "../store";
+import "./Task.css";
+
+export default function Task({ title }) {
+  const task = useStore((store) =>
+    store.tasks.find((task) => task.title === title)
+  );
+  return (
+    <div className="task">
+      <div>{task.title}</div>
+      <div className="bottomWrapper">
+        <div></div>
+        <div className={classNames("status", task.state)}>{task.state}</div>
+      </div>
+    </div>
+  );
+}
