c464d76eb353270a992c9f465152dad186594024
juleni <69912179+juleni@users.noreply.github.com>
2023-08-30 16:33:18 +0200

    Added addTask to the store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { createWithEqualityFn } from "zustand/traditional";
 
 const store = (set) => ({
   tasks: [{ title: "TestTask", state: "DONE" }],
+  addTask: (title, state) =>
+    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
 });
 
 export const useStore = createWithEqualityFn(store);
