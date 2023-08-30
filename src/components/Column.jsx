bb7bac9e95bdd578bf56e2b407a6093a54065eec
juleni <69912179+juleni@users.noreply.github.com>
2023-08-30 15:22:53 +0200

    Added  impl. for getting filtered tasks from store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,5 +1,50 @@
+import { shallow } from "zustand/shallow";
+import { useStore } from "../store";
 import "./Column.css";
+import Task from "./Task";
 
 export default function Column({ state }) {
-  return <div className="column">{state}</div>;
+  /** Comparison function options:
+   * 1. Use useMemo
+   * 2. Use filter function only with 'shallow' or with own comparison function
+   * Otherwise the array/object will be always rendered - because it doesn't compare
+   * particular attributes of the array/object
+  // OPTION 1
+  const tasks = useStore((store) => store.tasks);
+  const filtered = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
+
+  // OPTION 2
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+
+  // OPTION 3
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    (prev, next) => {
+      const longest = prev.length > next.length ? prev.length : next.length;
+      for (let i = 0; i < longest; i++) {
+        if (!prev[i] || !next[i]) return false;
+        if (prev[i] !== next[i]) return false;
+      }
+    }
+  );
+    */
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+
+  return (
+    <div className="column">
+      <p>{state}</p>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+    </div>
+  );
 }
