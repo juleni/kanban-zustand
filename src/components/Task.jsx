940bd597dc581f4299038111af6092ec6e4df3bc
juleni <69912179+juleni@users.noreply.github.com>
2023-08-31 10:21:01 +0200

    Added deleteTask

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import trashIcon from "../assets/trash.svg";
 import { useStore } from "../store";
 import "./Task.css";
 
@@ -6,11 +7,18 @@ export default function Task({ title }) {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
+  const deleteTask = useStore((store) => store.deleteTask);
+
   return (
     <div className="task">
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <img
+            src={trashIcon}
+            onClick={() => deleteTask(task.title)}
+          />
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
